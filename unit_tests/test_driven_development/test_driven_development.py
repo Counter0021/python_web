0f910708468659a6fa04ab26aad17343336847c3
Counter0021 <ara123rus123ara@gmail.com>
2021-03-25 13:33:45 +0300

    Studied test driven development(TDD) and unittest method

--- unit_tests/test_driven_development/test_driven_development.py
+++ unit_tests/test_driven_development/test_driven_development.py
@@ -1,10 +1,14 @@
 # TDD - Test Driven Development
 # Разработка приложений, управляемая тестами
 # В начале пишем тесты
+from random import choice
 
 
 # Приветствовать
 def greet(name, is_enemy):
+    # Утверждаем, что если is_enemy не bool объект
+    if not isinstance(is_enemy, bool):
+        raise ValueError('is_enemy must be a boolean type')
     if is_enemy:
         return f'Hello {name}! I will kill you, bastard!'
     else:
@@ -17,3 +21,16 @@ def eat_burgers(number):
         return 'Oh! I overate!'
     else:
         return 'Mmm! That was excellent!'
+
+
+# Летать
+def can_fly(name):
+    if name == 'Batman':
+        return True
+    else:
+        return False
+
+
+# Получить оружие
+def get_arsenal():
+    return choice(('knife', 'handgun', 'machine gun'))
--- unit_tests/test_driven_development/test_tdd.py
+++ unit_tests/test_driven_development/test_tdd.py
@@ -2,10 +2,11 @@
 # python test_tdd.py -v
 # Указать все функции(ОК) и вывести документацию
 import unittest
-from test_driven_development import greet, eat_burgers
+from test_driven_development import greet, eat_burgers, can_fly, get_arsenal
 
 
 class CoolGameFunctionsTest(unittest.TestCase):
+    # Приветствие положительному персонажу
     def test_greet(self):
         """
         greet() have to return 'How are you?' if is_enemy == False
@@ -13,14 +14,38 @@ class CoolGameFunctionsTest(unittest.TestCase):
         # Равенство
         self.assertEqual(greet('Krost', False), 'Hello Krost! How are you?')
 
+    # Приветствие врагу
     def test_greet_enemy(self):
         self.assertEqual(greet('Acsill', True), 'Hello Acsill! I will kill you, bastard!')
 
+    # Возникает ли ошибка?
+    def test_greet_enemy_boolean(self):
+        with self.assertRaises(ValueError):
+            greet('Ironside', 'XD')
+
+    # Поедание бургеров, когда <= 3
     def test_eat_burgers(self):
         self.assertEqual(eat_burgers(2), 'Mmm! That was excellent!')
 
+    # Поедание бургеров, когда > 3
     def test_eat_burgers_overate(self):
         self.assertEqual(eat_burgers(5), 'Oh! I overate!')
+
+    # Бэтмен умеет летать
+    def test_can_fly_batman(self):
+        self.assertTrue(can_fly('Batman'), msg='Batman have to be able to fly')
+
+    # Не бэтмен не умеет летать
+    def test_can_fly_anyone_else(self):
+        self.assertEqual(can_fly('Joker'), False)
+        self.assertEqual(can_fly('Acsill'), False)
+        self.assertEqual(can_fly('Krost'), False)
+        # Лучше так не делать!
+        # self.assertFalse(can_fly('Joker'), msg='Batman have to be able to fly')
+
+    # Получить арсенал
+    def test_get_arsenal(self):
+        self.assertIn(get_arsenal(), ('knife', 'handgun', 'machine gun'))
 
 
 if __name__ == '__main__':

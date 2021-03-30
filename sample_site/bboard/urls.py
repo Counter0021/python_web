38bd8dff2db27bee7770517b7d3fa43f1199ab29
Counter0021 <ara123rus123ara@gmail.com>
2021-03-30 14:09:47 +0300

    Studied how classes work with forms

--- sample_site/bboard/urls.py
+++ sample_site/bboard/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, BbByRubricView, BbCreateView, BbDetailView
+from .views import index, BbByRubricView, BbAddView, BbDetailView, BbEditView
 
 urlpatterns = [
     path('detail/<int:pk>/', BbDetailView.as_view(), name='detail'),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('<int:rubric_id>/', BbByRubricView.as_view(), name='by_rubric'),
     # path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', index, name='index'),
-    path('add/', BbCreateView.as_view(), name='add'),
+    path('add/', BbAddView.as_view(), name='add'),
+    path('update/<int:pk>/', BbEditView.as_view(), name='update')
 ]
--- sample_site/bboard/views.py
+++ sample_site/bboard/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, FormView, UpdateView
+from django.urls import reverse_lazy, reverse
 
 from .models import Bb, Rubric
 
@@ -9,6 +9,45 @@ from .forms import BbForm
 from django.views.generic.detail import DetailView
 
 from django.views.generic.list import ListView
+
+
+# Исправление объявления
+class BbEditView(UpdateView):
+    model = Bb
+    form_class = BbForm
+    success_url = '/bboard/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+
+# Добавление нового объявления
+class BbAddView(FormView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    initial = {'price': 0.0}
+
+    # Контекст шаблона
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+    # Валидация формы, сохранение её
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    # Получить форму, чтобы перенаправить потом на страницу с данной катигорией
+    def get_form(self, form_class=None):
+        self.object = super().get_form(form_class)
+        return self.object
+
+    # Перенаправить на url категорий
+    def get_success_url(self):
+        return reverse('by_rubric', kwargs={'rubric_id': self.object.cleaned_data['rubric'].pk})
 
 
 # Вывод объявлений из рубрики
@@ -36,22 +75,22 @@ class BbDetailView(DetailView):
         return context
 
 
-# Контроллер класс, для обработки формы
-class BbCreateView(CreateView):
-    # Путь к файлу шаблона, создающего страницу с формой
-    template_name = 'bboard/create.html'
-    # Ссылка на класс формы, связанной с моделью
-    form_class = BbForm
-    # Интернет-адрес для перенаправления после успешного сохранения данных
-    success_url = reverse_lazy('index')
-
-    # Вывод перечня рубрик, он формирует контекст шаблона
-    def get_context_data(self, **kwargs):
-        # Контекст шаблона
-        context = super().get_context_data(**kwargs)
-        # Добавляем список рубрик
-        context['rubrics'] = Rubric.objects.all()
-        return context
+# # Контроллер класс, для обработки формы
+# class BbCreateView(CreateView):
+#     # Путь к файлу шаблона, создающего страницу с формой
+#     template_name = 'bboard/create.html'
+#     # Ссылка на класс формы, связанной с моделью
+#     form_class = BbForm
+#     # Интернет-адрес для перенаправления после успешного сохранения данных
+#     success_url = reverse_lazy('index')
+#
+#     # Вывод перечня рубрик, он формирует контекст шаблона
+#     def get_context_data(self, **kwargs):
+#         # Контекст шаблона
+#         context = super().get_context_data(**kwargs)
+#         # Добавляем список рубрик
+#         context['rubrics'] = Rubric.objects.all()
+#         return context
 
 
 # # Страница с рубрикой

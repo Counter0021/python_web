1b9bc998f891cb5ad25debb6c00e644234f213cf
Counter0021 <ara123rus123ara@gmail.com>
2021-04-13 16:43:45 +0300

    Add sent email notification

--- bboard/main/utilities.py
+++ bboard/main/utilities.py
@@ -25,3 +25,16 @@ def send_activation_notification(user):
 # Генерация имён сохраняемых в модели выгруженных файлов
 def get_timestamp_path(instance, filename):
     return f'{datetime.now().timestamp()}{splitext(filename)[1]}'
+
+
+# Отправка уведомлений о новых комментариях
+def send_new_comment_notification(comment):
+    if ALLOWED_HOSTS:
+        host = 'http://' + ALLOWED_HOSTS[0]
+    else:
+        host = 'http://localhost:8000'
+    author = comment.bb.author
+    context = {'author': author, 'host': host, 'comment': comment}
+    subject = render_to_string('email/new_comment_letter_subject.txt', context)
+    body = render_to_string('email/new_comment_letter_body.txt', context)
+    author.email_user(subject, body)
